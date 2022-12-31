8947d8362d8e36c147d32ec98a563dc0c7e2ed40
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-31 12:52:02 +0300

    Starting Up Bot

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module Discord-Bot
 
 go 1.19
+
+require (
+	github.com/bwmarrin/discordgo v0.26.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package Discord_Bot
+package main
+
+import (
+	"Discord-Bot/bot"
+	"Discord-Bot/config"
+	"fmt"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+	<-make(chan struct{})
+	return
+}
